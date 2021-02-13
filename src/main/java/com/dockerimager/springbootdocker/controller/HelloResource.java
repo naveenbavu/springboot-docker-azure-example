8b686ec043f40518494d04140ba2f38dbc2fa3bd
Naveen <naveenbavu@gmail.com>
2021-02-12 23:32:17 -0800

    new spring boot + azure + container registry + docker example

---
+++ src/main/java/com/dockerimager/springbootdocker/controller/HelloResource.java
@@ -0,0 +1,20 @@
+package com.dockerimager.springbootdocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Naveen Bavu
+ */
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResource {
+
+  @GetMapping
+  public String get() {
+    return "Hello World";
+  }
+
+
+}
---
+++ src/test/java/com/dockerimager/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerimager.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
